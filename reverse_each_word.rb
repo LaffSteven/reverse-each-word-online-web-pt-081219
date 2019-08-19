6ef22ca38612a329ee025cf797c93256f29b8cfc
Steven Laff <steve.laff@gmail.com>
2019-08-19 19:00:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   s_array = sentence.split(" ")
   count = 0
   s_array.map do |word|
-    s_array[0]word.reverse
+    s_array[0] = word.reverse
     count += 1
   end
   s_array.join
