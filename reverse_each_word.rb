306c50d34d6590494c0158b26e9194050d0f127a
Steven Laff <steve.laff@gmail.com>
2019-08-19 19:16:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence)
   s_array = sentence.split(" ")
   reversed_word = ""
   s_array.map do |word|
-    reversed_word << s_array[s_array.index(word)].reverse
+    reversed_word << " #{s_array[s_array.index(word)].reverse}"
   end
 end
